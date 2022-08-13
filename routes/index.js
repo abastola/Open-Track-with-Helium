c3b437c9283b1342801948dd4fbaae2ab666ff7e
Anuj Bastola <abastol1@ramapo.edu>
2022-08-13 19:48:16 -0400

    Merge fixes from arjun-dev

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var database = require("../database")
 var router = express.Router();
 
 /* GET home page. */
@@ -6,22 +7,6 @@ router.get("/", function (req, res, next) {
   res.render("homepage");
 });
 
-/* GET tracker page */
-router.get("/tracker", function (req, res, next) {
-  var locations = [
-    ["8/11/2022, 8:11:59 PM", 11.8166, 122.0942],
-    ["8/11/2022, 8:13:59 PM", 11.8202, 122.0621],
-    ["8/11/2022, 8:12:59 PM", 11.8804, 122.0189],
-    ["8/11/2022, 8:16:59 PM", 11.8929, 122.0125]
-  ];
-
-  var responseData = {
-    title: "John Doe's Location",
-    locations: locations,
-  };
-
-  res.render("index", { Response: responseData });
-});
 
 /* GET home page. */
 router.get("/tracker/:userurl", async function (req, res, next) {
