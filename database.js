c29d4d07dc33acda2b2671fc503162e366d46659
Bibhash Mulmi <bmulmi@ramapo.edu>
2022-08-13 11:26:10 -0400

    Database connection string use env variable. Add create table if not exist in db build.

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 var pg = require("pg");
 
-var connString =
-  "";
+var connString = process.env.POSTGRES_CONNSTRING;
 var client = new pg.Client(connString);
 
-exports.build = function () {
+build = function () {
   client.connect(function (err) {
     if (err) {
       return console.error("could not connect to postgres", err);
@@ -19,32 +19,29 @@ exports.build = function () {
     });
   });
 
-  //   client.query(
-  //     "CREATE TABLE IF NOT EXISTS Device ( DeviceID INT NOT NULL PRIMARY KEY, Latitude DECIMAL(8,6) NULL, Longitude DECIMAL(9, 6) NULL, Time TIMESTAMP NULL)",
-  //     function (err, result) {
-  //       if (err) {
-  //         return console.error("error running query", err);
-  //       }
-  //       client.end();
-  //     }
-  //   );
+  var query =
+    "CREATE TABLE IF NOT EXISTS Device (ID SERIAL PRIMARY KEY, DeviceID INT NOT NULL, Latitude FLOAT NULL, Longitude FLOAT NULL, Time TIMESTAMP NULL)";
 
-  //   client.query(
-  //     "CREATE TABLE IF NOT EXISTS DeviceURL ( DeviceID INT NOT NULL PRIMARY KEY, URL VARCHAR(2048) NULL)",
-  //     function (err, result) {
-  //       if (err) {
-  //         return console.error("error running query", err);
-  //       }
-  //       client.end();
-  //     }
-  //   );
+  client.query(query, function (err, result) {
+    if (err) {
+      return console.error("error running query: ", query, err);
+    }
+  });
+
+  query =
+    "CREATE TABLE IF NOT EXISTS DeviceURL ( DeviceID INT NOT NULL PRIMARY KEY, URL VARCHAR(2048) NULL)";
+  client.query(query, function (err, result) {
+    if (err) {
+      return console.error("error running query: ", query, err);
+    }
+  });
 };
 
 /**
  *
  * @param {json} deviceData .DeviceID
  */
-exports.getDeviceData = (deviceData, callback) => {
+getDeviceData = (deviceData, callback) => {
   console.log(
     "Query: ",
     `SELECT * FROM Device where DeviceID = ${deviceData.DeviceID};`
@@ -74,9 +71,34 @@ exports.getDeviceData = (deviceData, callback) => {
  * @param {*} deviceData  {DeviceID: <ID>}
  * @param {*} callback (error, {URL: 'url'})
  */
-exports.getDeviceURL = (deviceData, callback) => {
+getDeviceURL = (deviceData, callback) => {
+  console.log(
+    `SELECT URL FROM DeviceURL where DeviceID = ${deviceData.DeviceID};`
+  );
   client.query(
-    `SELECT URL FROM  DeviceURL where DeviceID = ${deviceData.DeviceID};`,
+    `SELECT URL FROM DeviceURL where DeviceID = ${deviceData.DeviceID};`,
+    (error, results) => {
+      if (error) {
+        console.log(error);
+        throw error;
+      } else if (results.rows.length > 1) {
+        console.log("Multiple records found!");
+        callback(null, null);
+      } else {
+        callback(null, results.rows[0]);
+      }
+    }
+  );
+};
+
+/**
+ *
+ * @param {*} deviceData {URL : 'url'}
+ * @param {*} callback (err, result)
+ */
+getDeviceID = (deviceData, callback) => {
+  client.query(
+    `SELECT DeviceID FROM DeviceURL where URL = '${deviceData.URL}';`,
     (error, results) => {
       if (error) {
         console.log(error);
@@ -95,7 +117,7 @@ exports.getDeviceURL = (deviceData, callback) => {
  *
  * @param {json} deviceData {DeviceID: , Latitude: , Longitude: , Time: ''}
  */
-exports.addDeviceData = (deviceData) => {
+addDeviceData = (deviceData) => {
   client.query(
     `INSERT INTO Device(DeviceID, Latitude, Longitude, Time) VALUES (${deviceData.DeviceID}, ${deviceData.Latitude}, ${deviceData.Longitude}, '${deviceData.Time}')`,
     (error) => {
@@ -110,7 +132,7 @@ exports.addDeviceData = (deviceData) => {
  *
  * @param {json} deviceData {DeviceID: , URL: ''}
  */
-exports.addDeviceURL = (deviceData) => {
+addDeviceURL = (deviceData) => {
   console.log(
     `INSERT INTO DeviceURL (DeviceID, URL) VALUES (${deviceData.DeviceID}, '${deviceData.URL}')`
   );
@@ -126,9 +148,16 @@ exports.addDeviceURL = (deviceData) => {
   );
 };
 
-// build();
+build();
 // addDeviceURL({DeviceID:2, URL:'testur2'});
 // addDeviceData({DeviceID: 1, Latitude: 12.0, Longitude: 7.0, Time: '20220101'});
-// getDeviceURL({DeviceID: 1}, (e, r) => console.log("device url", r));
+// getDeviceURL({ DeviceID: 1 }, (e, r) => console.log("device url", r));
+getDeviceID({URL:'testurl'}, (e, r) => console.log("DeviceID: ", r));
 // getDeviceData({DeviceID: 1}, (e, r) => console.log("device data",r));
 // console.log("Device URL:", d.URL);
+
+exports.getDeviceData;
+exports.getDeviceID;
+exports.getDeviceURL;
+exports.addDeviceData;
+exports.addDeviceURL;
